2aeac7f41502ca4811b1733ae34e3a4b82fe3f4a
Chokkoramo Mac <perdomojuank300@gmail.com>
2026-03-20 03:35:52 -0500

    fix: Se corrigen archivos para la configuracion del agente

--- agente-docker/start.sh
+++ agente-docker/start.sh
@@ -13,21 +13,28 @@ fi
 
 AZP_POOL=${AZP_POOL:-Default}
 
-mkdir /azp
+mkdir -p /azp
 cd /azp
 
 echo "Descargando agente..."
-curl -LsS https://vstsagentpackage.azureedge.net/agent/3.236.1/vsts-agent-linux-x64-3.236.1.tar.gz | tar -xz
+curl -fLsS https://download.agent.dev.azure.com/agent/4.270.0/vsts-agent-linux-x64-4.270.0.tar.gz -o agent.tar.gz
 
-echo "Configurando agente..."
-./config.sh --unattended \
-  --agent "$(hostname)" \
-  --url "$AZP_URL" \
-  --auth pat \
-  --token "$AZP_TOKEN" \
-  --pool "$AZP_POOL" \
-  --work "_work" \
-  --replace
+tar -xzvf agent.tar.gz
+
+if [ ! -f .agent ]; then
+  echo "Configurando agente..."
+
+  ./config.sh --unattended \
+    --agent "$(hostname)" \
+    --url "$AZP_URL" \
+    --auth pat \
+    --token "$AZP_TOKEN" \
+    --pool "$AZP_POOL" \
+    --work "_work" \
+    --replace
+else
+  echo "Agente ya configurado, iniciando..."
+fi
 
 echo "Iniciando agente..."
 ./run.sh
