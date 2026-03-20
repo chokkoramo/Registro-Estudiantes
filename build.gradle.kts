0742e717ad7e75a1af8bd6248fc945ace65ba5cd
Chokkoramo Mac <perdomojuank300@gmail.com>
2026-03-20 15:10:11 -0500

    fix: Se arregla URL de sonar para ejecutar la tarea con el agente

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ sonar {
     properties {
         property("sonar.projectKey", "Registro-Estudiantes")
         property("sonar.projectName", "Registro Estudiantes")
-        property("sonar.host.url", "http://localhost:9000" )
+        property("sonar.host.url", "http://sonarqube:9000" )
         property("sonar.token", project.findProperty("sonarToken") ?: "")
     }
 }
