ce2ecc36ead4f03f7d53d2de4d4f043ae6022767
JuanCaaa <perdomojuank300@gmail.com>
2026-03-25 00:47:45 -0500

    Se elimina un archivo obsoleto

--- src/test/java/runners/RunCucumberTest.java
+++ src/test/java/runners/RunCucumberTest.java
@@ -1,8 +1,15 @@
 package runners;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 
 }
